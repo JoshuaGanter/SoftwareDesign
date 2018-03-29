7fde9b25ee39f466228c3329d43a780b8b1abb24
Joshua Ganter <joshua.michael.ganter@hs-furtwangen.de>
2018-03-30 00:37:12 +0200

    Finished tasks 1.1, 1.2

---
+++ L02/A01_Koerpereigenschaften/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Linq;
+
+namespace A01_Koerpereigenschaften
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 2)
+            {
+                string koerper = args[0];
+                if(koerper == "w" || koerper == "k" || koerper == "o")
+                {
+                    try
+                    {
+                        double d = double.Parse(args[1]);
+
+                        switch(koerper)
+                        {
+                            case "w":
+                                Console.WriteLine(GetCubeInfo(d));
+                                break;
+                            case "k":
+                                Console.WriteLine(GetSphereInfo(d));
+                                break;
+                            case "o":
+                                Console.WriteLine(GetOctahedronInfo(d));
+                                break;
+                        }
+                    }
+                    catch(FormatException e)
+                    {
+                        Console.WriteLine("Keine gültige Kantenlänge/kein gültiger Durchmesser.");
+                        Console.WriteLine(e.Message);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Kein gültiger Körper.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Die Zahl der Argumente ist ungültig.");
+            }
+        }
+
+        static double GetCubeSurface(double d)
+        {
+            return 6 * Math.Pow(d, 2);
+        }
+        static double GetCubeVolume(double d)
+        {
+            return Math.Pow(d, 3);
+        }
+        static string GetCubeInfo(double d)
+        {
+            return "Kubus: A=" + Math.Round(GetCubeSurface(d), 2) + " | V=" + Math.Round(GetCubeVolume(d), 2);
+        }
+
+        static double GetSphereSurface(double d)
+        {
+            return Math.PI * Math.Pow(d, 2);
+        }
+        static double GetSphereVolume(double d)
+        {
+            return Math.PI * Math.Pow(d, 3) / 6;
+        }
+        static string GetSphereInfo(double d)
+        {
+            return "Kugel: A=" + Math.Round(GetSphereSurface(d), 2) + " | V=" + Math.Round(GetSphereVolume(d), 2);
+        }
+
+        static double GetOctahedronSurface(double d)
+        {
+            return 2 * Math.Sqrt(3) * Math.Pow(d, 2);
+        }
+        static double GetOctahedronVolume(double d)
+        {
+            return Math.Sqrt(2) * Math.Pow(d, 3) / 3;
+        }
+        static string GetOctahedronInfo(double d)
+        {
+            return "Oktaeder: A=" + Math.Round(GetOctahedronSurface(d), 2) + " | V=" + Math.Round(GetOctahedronVolume(d), 2);
+        }
+    }
+}
---
+++ L02/A01_Zufallsgedicht/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace A01_Zufallsgedicht
+{
+    class Program
+    {
+        static string[] subjects = { "Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore" };
+        static string[] verbs = { "braut", "liebt", "studiert", "hasst", "zaubert", "zerstört" };
+        static string[] objects = { "Zaubertränke", "den Grimm", "Lupin", "Hogwards", "die Karte des Rumtreibers", "Dementoren" };
+
+        static Random random = new Random();
+
+        static void Main(string[] args)
+        {
+            string[] poem = new string[5];
+            for (int i = 0; i < poem.Length; i++)
+            {
+                poem[i] = GetVerse();
+            }
+            
+            foreach (string line in poem)
+            {
+                Console.WriteLine(line);
+            }
+        }
+
+        static string GetVerse()
+        {
+            string verse_subject = subjects[random.Next(subjects.Length - 1)];
+            subjects = subjects.Where(val => val != verse_subject).ToArray();
+
+            string verse_verb = verbs[random.Next(verbs.Length - 1)];
+            verbs = verbs.Where(val => val != verse_verb).ToArray();
+
+            string verse_object = objects[random.Next(objects.Length - 1)];
+            objects = objects.Where(val => val != verse_object).ToArray();
+            
+            return verse_subject + " " + verse_verb + " " + verse_object;
+        }
+    }
+}
