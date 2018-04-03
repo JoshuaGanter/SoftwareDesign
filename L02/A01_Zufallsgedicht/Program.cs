3646beac422d5c28b28075a5d25e847738aeef81
Joshua Ganter <joshua.michael.ganter@hs-furtwangen.de>
2018-04-03 22:11:38 +0200

    added comments

--- L02/A01_Zufallsgedicht/Program.cs
+++ L02/A01_Zufallsgedicht/Program.cs
@@ -5,15 +5,18 @@ namespace A01_Zufallsgedicht
 {
     class Program
     {
+        // Literal initialization of subjects, verbs, objects. The arrays must contain more items than poem_length.
         static string[] subjects = { "Harry", "Hermine", "Ron", "Hagrid", "Snape", "Dumbledore" };
         static string[] verbs = { "braut", "liebt", "studiert", "hasst", "zaubert", "zerstört" };
         static string[] objects = { "Zaubertränke", "den Grimm", "Lupin", "Hogwards", "die Karte des Rumtreibers", "Dementoren" };
+
+        static uint poem_length = 5;
 
         static Random random = new Random();
 
         static void Main(string[] args)
         {
-            string[] poem = new string[5];
+            string[] poem = new string[poem_length];
             for (int i = 0; i < poem.Length; i++)
             {
                 poem[i] = GetVerse();
@@ -27,9 +30,13 @@ namespace A01_Zufallsgedicht
 
         static string GetVerse()
         {
+            // Get a random subject.
             string verse_subject = subjects[random.Next(subjects.Length - 1)];
+
+            // Remove the subject from the subjects list by constructing a new array with the subjects not equal to the subject used.
             subjects = subjects.Where(val => val != verse_subject).ToArray();
 
+            // Repeat for verbs and objects.
             string verse_verb = verbs[random.Next(verbs.Length - 1)];
             verbs = verbs.Where(val => val != verse_verb).ToArray();
 
