28ef0c909b53f176baec628fa9ea031ea9ae5839
Joshua Ganter <joshua.michael.ganter@hs-furtwangen.de>
2018-04-10 12:43:59 +0200

    Added TODOs for Lecture 3

---
+++ L03/TODO/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace TODO
+{
+
+    class Person
+    {
+        public string name { get; private set; }
+        public int age { get; private set; }
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"Mein Name ist {name} und ich bin {age} Jahre alt.";
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person horst = new Person("Horst", 42);
+
+            Person[] people = {
+                horst,
+                new Person("Dieter", 66),
+                new Person("Karl", 32),
+                new Person("Erich", 79),
+                new Person("Ernst", 55)
+            };
+
+            foreach (Person person in people)
+            {
+                DecideIfPersonIsOld(person);
+            }
+        }
+
+        private static void DecideIfPersonIsOld(Person person)
+        {
+            if (person.age > 40)
+            {
+                Console.WriteLine("Du bist alt.");
+            }
+            else
+            {
+                Console.WriteLine("On the right side of fourty.");
+            }
+        }
+    }
+}
