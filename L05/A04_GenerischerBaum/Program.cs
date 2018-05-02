5bff48e24ec2d8d5782abb968fd28288faaaff29
Joshua Ganter <joshua.michael.ganter@hs-furtwangen.de>
2018-05-02 12:50:45 +0200

    Made proposed changes, added Find method

--- L05/A04_GenerischerBaum/Program.cs
+++ L05/A04_GenerischerBaum/Program.cs
@@ -4,21 +4,54 @@ using System.Linq;
 
 namespace A04_GenerischerBaum
 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var root = new TreeNode<string>("I am the Godfather.");
+            var child1 = root.CreateNode("abc", root);
+            var child2 = root.CreateNode("Hey, me too!", root);
+            var gchild11 = root.CreateNode("abc", child1);
+            var gchild12 = root.CreateNode("Another brick in the wall.", child1);
+            var ggchild121 = root.CreateNode("I'm still an ancestor of the Godfather!", gchild12);
+            var gggchild1211 = root.CreateNode("abc", ggchild121);
+
+            root.PrintTree();
+
+            Console.WriteLine("");
+            ggchild121.RemoveChild(gggchild1211);
+            gchild11.AppendChild(gggchild1211);
+            root.PrintTree();
+
+            foreach (var element in root.Find("abc"))
+            {
+                Console.WriteLine(element);
+            }
+
+            // This would cause an infinite loop, since the child element (re)refers to it's root.
+            //gggchild1211.AppendChild(root);
+            //root.PrintTree();
+        }
+    }
+
     class TreeNode<T>
     {
         private T _Content;
         private TreeNode<T> Parent;
         private List<TreeNode<T>> Children = new List<TreeNode<T>>();
 
-        public TreeNode(T content)
+        public TreeNode(T content, TreeNode<T> parent = null)
         {
             _Content = content;
+            if (parent != null)
+            {
+                parent.AppendChild(this);
+            }
         }
 
-        public TreeNode(T content, TreeNode<T> parent)
+        public TreeNode<T> CreateNode(T content, TreeNode<T> parent = null)
         {
-            _Content = content;
-            parent.AppendChild(this);
+            return new TreeNode<T>(content, parent);
         }
 
         public void AppendChild(TreeNode<T> node)
@@ -32,61 +65,48 @@ namespace A04_GenerischerBaum
             Children.Remove(node);
         }
 
-        public void PrintTree()
+        public void PrintTree(string prefix = "")
         {
-            Console.WriteLine(GetTreeString());
-        }
-
-        private string GetTreeString(string prefix = "")
-        {
-            string result = _Content.ToString();
+            Console.WriteLine(ToString());
 
             if (Children.Count != 0)
             {
                 TreeNode<T> lastChild = Children.Last();
                 foreach (TreeNode<T> child in Children)
                 {
-                    if (lastChild == child)
+                    if (child == lastChild)
                     {
-                        result += "\n" + prefix + "└─ " + child.GetTreeString(prefix + "   ");
+                        Console.Write(prefix + "└─ ");
+                        child.PrintTree(prefix + "   ");
                     }
                     else
                     {
-                        result += "\n" + prefix + "├─ " + child.GetTreeString(prefix + "|  ");
+                        Console.Write(prefix + "├─ ");
+                        child.PrintTree(prefix + "|  ");
                     }
                 }
             }
-            return result;
         }
 
         public override string ToString()
         {
             return _Content.ToString();
         }
-    }
 
-    class Program
-    {
-        static void Main(string[] args)
+        public List<TreeNode<T>> Find(T searchFor, List<TreeNode<T>> result = null)
         {
-            var root = new TreeNode<string>("I am the Godfather.");
-            var child1 = new TreeNode<string>("And I'm a son of him.", root);
-            var child2 = new TreeNode<string>("Hey, me too!", root);
-            var gchild11 = new TreeNode<string>("And I'm the daugther of a son of the Godfather. Yay!", child1);
-            var gchild12 = new TreeNode<string>("Another brick in the wall.", child1);
-            var ggchild121 = new TreeNode<string>("I'm still an ancestor of the Godfather!", gchild12);
-            var gggchild1211 = new TreeNode<string>("I'm just a regular guy. U mad?", ggchild121);
-
-            root.PrintTree();
-
-            Console.WriteLine("");
-            ggchild121.RemoveChild(gggchild1211);
-            gchild11.AppendChild(gggchild1211);
-            root.PrintTree();
-
-            // This would cause an infinite loop, since the child element (re)refers to it's root.
-            //gggchild1211.AppendChild(root);
-            //root.PrintTree();
+            if (result == null)
+            {
+                result = new List<TreeNode<T>>();
+            }
+            if (_Content.Equals(searchFor)) {
+                result.Add(this);
+            }
+            foreach(var child in Children)
+            {
+                child.Find(searchFor, result);
+            }
+            return result;
         }
     }
 }
